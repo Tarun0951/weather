bfd5f71f486bc5a101e3c15565a5cea39e6f5ea0
Baswa Tarun <113555184+Tarun0951@users.noreply.github.com>
2023-06-23 21:05:47 +0530

    Update OpenWeatherService.js

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,4 +1,4 @@
-const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions';
+const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
 const WEATHER_API_KEY = 'f0ea19bd75f15e40ad09f16011f4f4f3';
